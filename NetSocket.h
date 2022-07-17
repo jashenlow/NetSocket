b089730143648b74555498fbd582fafedd61ea01
Jashen Low <jashenlow@gmail.com>
2022-07-17 19:05:28 +0800

    [Modified]   - Changed license to MIT license.

--- NetSocket.h
+++ NetSocket.h
@@ -1,12 +1,27 @@
-// The following ifdef block is the standard way of creating macros which make exporting
-// from a DLL simpler. All files within this DLL are compiled with the NETSOCKETMANAGER_EXPORTS
-// symbol defined on the command line. This symbol should not be defined on any project
-// that uses this DLL. This way any other project whose source files include this file see
-// NETSOCKETMANAGER_API functions as being imported from a DLL, whereas this DLL sees symbols
-// defined with this macro as being exported.
+//Copyright(c) 2022 Jashen Low
+//
+//Permission is hereby granted, free of charge, to any person obtaining
+//a copy of this softwareand associated documentation files(the
+//	"Software"), to deal in the Software without restriction, including
+//	without limitation the rights to use, copy, modify, merge, publish,
+//	distribute, sublicense, and /or sell copies of the Software, and to
+//	permit persons to whom the Software is furnished to do so, subject to
+//	the following conditions :
+//
+//The above copyright noticeand this permission notice shall be
+//included in all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+//LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+//WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 #pragma once
-#ifndef _NET_SOCKET_MANAGER_H_
-#define _NET_SOCKET_MANAGER_H_
+#ifndef _NETSOCKET_H_
+#define _NETSOCKET_H_
 
 #ifdef NETSOCKETMANAGER_EXPORTS
 #define NETSOCKETMANAGER_API __declspec(dllexport)
