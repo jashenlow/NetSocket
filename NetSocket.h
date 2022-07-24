a9be3318d95b3683f413edc90d1a00be1aa00335
Jashen Low <jashenlow@gmail.com>
2022-07-24 22:06:43 +0800

    [Modified]   - Did an overhaul of the NetSocket class, which is intended to be cross-platform compatible.   - NetSocket is now a header-only library.
    [Added]
      - NetUtil class in a single header file, used to perform byte-swapping operations for various data types.
    
    Things that are incomplete:
      - All unix-specific implementations.
      - TCP implementation.
      - IPv6 compatibility.

--- NetSocket.h
+++ NetSocket.h
@@ -23,23 +23,38 @@
 #ifndef _NETSOCKET_H_
 #define _NETSOCKET_H_
 
-#ifdef NETSOCKETMANAGER_EXPORTS
-#define NETSOCKETMANAGER_API __declspec(dllexport)
-#else
-#define NETSOCKETMANAGER_API __declspec(dllimport)
-#endif
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(_WIN64) || defined(__NT__)
+#define SOCKET_PLATFORM_WINDOWS
 
 #define WIN32_LEAN_AND_MEAN
 #include <WS2tcpip.h>
 #pragma comment(lib, "Ws2_32.lib")
 
-#include <map>
+#define GetSocketError (WSAGetLastError())
+#elif defined(__APPLE__) || defined(__linux__) || defined(__unix__)
+#define SOCKET_PLATFORM_UNIX
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define GetSocketError (errno)
+#endif
+
 #include <string>
+#include <vector>
 
-constexpr uint16_t BUFFER_DEFAULT_SIZE = 4096;
-constexpr uint16_t TCP_MAX_CONNECTIONS = 100;
+constexpr uint16_t	BUFFER_DEFAULT_SIZE	= 4096;
+constexpr uint16_t	MAX_UDP_BUFFER_SIZE	= 65507;
+constexpr uint32_t	MAX_TCP_BUFFER_SIZE	= 1048576;
 
-enum class SocketError : uint8_t
+#ifdef SOCKET_PLATFORM_WINDOWS
+enum class WinsockError
 {
 	OK,
 	FAIL_WINSOCK,
@@ -47,80 +62,450 @@ enum class SocketError : uint8_t
 	FAIL_BIND,
 	LIMIT_REACHED
 };
+#endif
 
-enum class SocketBlockFlag : uint8_t
+enum class SocketType
 {
-	BLOCKING,
-	NON_BLOCKING
+	UDP,
+	TCP_CLIENT,
+	TCP_SERVER
 };
 
-
-
-struct SocketStruct
-{
-	sockaddr_in LocalAddr;
-	char* TxBuffer = nullptr;
-	char* RxBuffer = nullptr;
-	uint16_t TxBufferSize = 0;
-	uint16_t RxBufferSize = 0;
-	SocketBlockFlag BlockingType;
-};
-
-struct UDPSocketStruct : SocketStruct
-{
-	sockaddr_in TargetAddr;
-	SOCKET Handle;
-};
-
-struct TCPSocketStruct : SocketStruct
-{
-	SOCKET LocalHandle;
-	std::map<SOCKET, sockaddr_in> Peers;
-	uint16_t PeerLimit;
-};
-
-class NETSOCKETMANAGER_API CNetSocketManager 
+class NetSocket
 {
 public:
-	CNetSocketManager(void);
-	~CNetSocketManager(void);
+	struct SocketInfo
+	{
+		size_t		SocketFD;
+		sockaddr_in Addr;
+		std::string IPAddress;
+		uint16_t	Port;
 
-	SOCKET InitSocketUDP(const char* ip, const uint16_t& port, const SocketBlockFlag& blockFlag);
-	bool InitSocketUDP(SOCKET socket, const char* ip, const uint16_t& port, const SocketBlockFlag& blockFlag);
+		SocketInfo()
+		{
+			Reset();
+		}
+
+		void Reset()
+		{
+			SocketFD = 0;
+
+			Addr.sin_family = AF_INET;
+			memset(&Addr.sin_addr, 0, sizeof(Addr.sin_addr));
+			Addr.sin_port = 0;
+			memset(Addr.sin_zero, '\0', sizeof(Addr.sin_zero));
+
+			IPAddress.clear();
+			Port = 0;
+		}
+	};
+
+	struct LocalSocketInfo : SocketInfo
+	{
+		std::vector<SocketInfo> RemoteSocketInfo;	//This is used to store UDP sender info, TCP connected clients, and connected TCP server info.
+		SocketType	Type = SocketType::UDP;
+		IPPROTO		Protocol = IPPROTO::IPPROTO_UDP;
+	};
+
+	//Socket Constructor
+	NetSocket()
+	{
+		rxBuffer.resize(MAX_UDP_BUFFER_SIZE);
+		txBuffer.resize(BUFFER_DEFAULT_SIZE);
+
+		udpTargetAddr.sin_family = AF_UNSPEC;	//To indicate that this is not configured.
+		memset(&udpTargetAddr.sin_addr, 0, sizeof(udpTargetAddr.sin_addr));
+		udpTargetAddr.sin_port = 0;
+		memset(udpTargetAddr.sin_zero, '\0', sizeof(udpTargetAddr.sin_zero));
+	}
+
+	//Socket Destructor
+	~NetSocket()
+	{
+		CloseSocket();
+	}
+
+	//Get all info about this socket
+	const LocalSocketInfo& GetThisSocketInfo() const
+	{
+		return thisSocketInfo;
+	}
+
+	/*
+	Returns a value which determines if the specified socket is using IPv4 or IPv6.
+	Return values: AF_INET or AF_INET6.
+	*/
+	const uint16_t& GetIPVersion(const SocketInfo& socket) const
+	{
+		return thisSocketInfo.Addr.sin_family;
+	}
+
+	//Get transmit buffer size
+	const uint32_t& GetTxBufferSize() const
+	{
+		return (uint32_t)txBuffer.size();
+	}
+
+	//Set transmit buffer size
+	void SetTxBufferSize(const uint32_t& newSize)
+	{
+		if (newSize != txBuffer.size())
+			txBuffer.resize((newSize > maxBufferSize) ? maxBufferSize : newSize);
+	}
+
+#ifdef SOCKET_PLATFORM_WINDOWS
+	//Initialize socket
+	WinsockError Init(const char* localIP, const uint16_t& localPort, const SocketType& type, const bool& isNonBlocking)
+	{
+		maxBufferSize			= (type == SocketType::UDP) ? MAX_UDP_BUFFER_SIZE : MAX_TCP_BUFFER_SIZE;
+		unsigned long blocking	= (unsigned long)isNonBlocking;
+		WinsockError errorcode	= WinsockError::OK;
+
+		//Set local socket details
+		thisSocketInfo.IPAddress	= localIP;
+		thisSocketInfo.Port			= localPort;
+		thisSocketInfo.Type			= type;
+		thisSocketInfo.Protocol		= (type == SocketType::UDP) ? IPPROTO::IPPROTO_UDP : IPPROTO::IPPROTO_TCP;
+
+		thisSocketInfo.Addr.sin_family = AF_INET;
+		inet_pton(AF_INET, localIP, &thisSocketInfo.Addr.sin_addr);
+		thisSocketInfo.Addr.sin_port = htons(localPort);
+
+		//Initialize Winsock
+		WSADATA wsData;
+		if (WSAStartup(MAKEWORD(2, 2), &wsData) != 0)
+			errorcode = WinsockError::FAIL_WINSOCK;
+		else
+		{
+			//Create socket
+			thisSocketInfo.SocketFD = socket(AF_INET, (thisSocketInfo.Protocol == IPPROTO::IPPROTO_TCP) ? SOCK_STREAM : SOCK_DGRAM, thisSocketInfo.Protocol);
+			
+			if (thisSocketInfo.SocketFD == INVALID_SOCKET)
+				errorcode = WinsockError::FAIL_SOCKET;
+			else
+			{
+				//Bind socket to localIP and localPort
+				if (bind(thisSocketInfo.SocketFD, (sockaddr*)&thisSocketInfo.Addr, sizeof(sockaddr_in)) == SOCKET_ERROR)
+					errorcode = WinsockError::FAIL_BIND;
+				else
+				{
+					//Set socket blocking flag
+					ioctlsocket(thisSocketInfo.SocketFD, FIONBIO, &blocking);
+
+					if (rxBuffer.empty())
+						SetRxBufferSize(maxBufferSize);
+					if (txBuffer.empty())
+						SetTxBufferSize(BUFFER_DEFAULT_SIZE);
+
+					thisSocketInfo.RemoteSocketInfo.clear();
+
+					if ((thisSocketInfo.Type == SocketType::UDP) || (thisSocketInfo.Type == SocketType::TCP_CLIENT))
+						thisSocketInfo.RemoteSocketInfo.resize(1);					
+				}
+			}
+		}
+
+		if (errorcode != WinsockError::OK)
+			CloseSocket();
+
+		return errorcode;
+	}
+
+	//Close the socket
+	bool CloseSocket() const
+	{
+		if (closesocket(thisSocketInfo.SocketFD) != SOCKET_ERROR)
+			return (WSACleanup() == 0);
+		else
+			return false;
+	}
+
+	/*
+	Attempts to connect to a TCP server if this socket is a TCP client.
+	Returns TRUE if connection is successful, FALSE if connection is unsuccessful or if this socket is a UDP socket.
+	*/
+	bool ConnectServerTCP(const char* serverIP, const uint16_t& serverPort)
+	{
+		if (thisSocketInfo.Type == SocketType::TCP_CLIENT)
+		{
+			SocketInfo& serverInfo = thisSocketInfo.RemoteSocketInfo.front();
+			
+			serverInfo.Addr.sin_family = AF_INET;
+			inet_pton(AF_INET, serverIP, &serverInfo.Addr.sin_addr);
+			serverInfo.Addr.sin_port = htons(serverPort);
+
+			return (connect(thisSocketInfo.SocketFD, (sockaddr*)&serverInfo.Addr, sizeof(sockaddr_in)) != SOCKET_ERROR);
+		}
+		else
+			return false;
+	}
+
+	//Check number of bytes waiting in the input buffer
+	inline const int& CheckReceivedBytes()
+	{
+		ioctlsocket(thisSocketInfo.SocketFD, FIONREAD, (u_long*)&recvBytes);
+		return recvBytes;
+	}
 	
-	SOCKET InitTCPClient(const char* ip, const uint16_t& port, const SocketBlockFlag& blockFlag);
-	bool InitTCPClient(SOCKET socket, const char* ip, const uint16_t& port, const SocketBlockFlag& blockFlag);
+	/*
+	Call this to send any data type through the network.
+	NOTE: For string types, ensure that dataSize = stringlength + 1.
+	NOTE: For Unicode strings, ensure that dataSize = (stringlength + 1) * charsize.
+	Arguments:
+	  - data:       Pointer to the data to be sent.
+	  - dataSize:   Size of the data to send.
+	  - tartgetIP:  IP address of the receiver.
+	  - targetPort: Port number of the receiver.
+	*/
+	template<typename T>
+	bool SendDataUDP(T* data, const size_t& dataSize, const char* targetIP, const uint16_t& targetPort)
+	{
+		if ((data != nullptr) && (thisSocketInfo.Protocol == IPPROTO::IPPROTO_UDP))
+		{
+			char* dataPtr = nullptr;
 
-	SOCKET InitTCPServer(const char* ip, const uint16_t& port, const SocketBlockFlag& blockFlag);
-	bool InitTCPServer(SOCKET socket, const char* ip, const uint16_t& port, const SocketBlockFlag& blockFlag);
+			udpTargetAddr.sin_family = AF_INET;
+			inet_pton(AF_INET, targetIP, &udpTargetAddr.sin_addr);
+			udpTargetAddr.sin_port = htons(targetPort);
 
-	bool CloseSocket(SOCKET socket);
-	bool CloseAllSockets();
+			if (IsUnicodeString(data))
+			{
+				if (dataSize > txBuffer.size())
+					txBuffer.resize(dataSize + sizeof(T));
 
-	uint16_t GetRxBufferSize(SOCKET socket);
-	void SetRxBufferSize(SOCKET socket, const uint16_t& newSize);
+				memset(txBuffer.data(), '\0', txBuffer.size());
+				memcpy(txBuffer.data(), data, dataSize);
+
+				dataPtr = (char*)txBuffer.data();
+			}
+			else
+				dataPtr = (char*)data;
+			
+			return (sendto(thisSocketInfo.SocketFD, dataPtr, (int)dataSize, 0, (sockaddr*)&udpTargetAddr, sizeof(sockaddr)) != SOCKET_ERROR);
+		}
+		else
+			return false;
+	}
+
+	/*
+	Call this function if the expected data has a fixed size(like structs), or if you simply want to copy the data into a buffer.
+	Returns the number of bytes received.
+	Arguments:
+	  - data:       Pointer to where you would like to store the received data.
+	  - dataSize:   The expected data size to be received. Set to SIZE_MAX if the expected data size is unknown.
+	                If set to SIZE_MAX, ensure that the "data" argument points to a buffer large enough to avoid potential overruns.
+	  - senderIP:   The IP address of the sender.
+	  - senderPort: The sender's port number.
+	*/
+	int ReceiveDataUDP(void* data, const size_t& dataSize, std::string& senderIP, uint16_t& senderPort)
+	{
+		if ((data != nullptr) && (thisSocketInfo.Protocol == IPPROTO::IPPROTO_UDP))
+		{
+			int udpSenderLen		= sizeof(sockaddr);
+			SocketInfo& senderInfo	= thisSocketInfo.RemoteSocketInfo.front();
+			
+			memset(rxBuffer.data(), '\0', rxBuffer.size());
+			recvBytes = recvfrom(thisSocketInfo.SocketFD, (char*)rxBuffer.data(), (int)rxBuffer.size(), 0, (sockaddr*)&senderInfo.Addr, &udpSenderLen);
+			
+			GetIPFromSockAddr(senderInfo);
+			senderIP = senderInfo.IPAddress;
+			GetPortFromSockAddr(senderInfo);
+			senderPort = senderInfo.Port;
+
+			if ((recvBytes > 0) && (recvBytes != SOCKET_ERROR))
+			{
+				//Check if all received data should be copied into the "data" argument without checking.
+				if (dataSize == SIZE_MAX)
+					memcpy(data, rxBuffer.data(), recvBytes);
+				else
+				{
+					//Check if received data size is the expected size.
+					if (recvBytes == dataSize)
+						memcpy(data, rxBuffer.data(), recvBytes);
+				}
+
+				return recvBytes;
+			}
+			else
+				return 0;
+		}
+		else
+			return 0;
+	}
+
+	/*
+	Call this function if the expected data has a fixed size(like structs), or if you simply want to copy the data into a buffer.
+	Returns the number of bytes received.
+	Arguments:
+	  - data:     Pointer to where you would like to store the received data.
+	  - dataSize: The expected data size to be received. Set to SIZE_MAX if the expected data size is unknown.
+			      If set to SIZE_MAX, ensure that the "data" argument points to a buffer large enough to avoid potential overruns.
+	*/
+	int ReceiveDataUDP(void* data, const size_t& dataSize)
+	{
+		if ((data != nullptr) && (thisSocketInfo.Protocol == IPPROTO::IPPROTO_UDP))
+		{
+			int udpSenderLen = sizeof(sockaddr);
+			SocketInfo& senderInfo = thisSocketInfo.RemoteSocketInfo.front();
+
+			memset(rxBuffer.data(), '\0', rxBuffer.size());
+			recvBytes = recvfrom(thisSocketInfo.SocketFD, (char*)rxBuffer.data(), (int)rxBuffer.size(), 0, (sockaddr*)&senderInfo.Addr, &udpSenderLen);
+
+			if ((recvBytes > 0) && (recvBytes != SOCKET_ERROR))
+			{
+				//Check if all received data should be copied into the "data" argument without checking.
+				if (dataSize == SIZE_MAX)
+					memcpy(data, rxBuffer.data(), recvBytes);
+				else
+				{
+					//Check if received data size is the expected size.
+					if (recvBytes == dataSize)
+						memcpy(data, rxBuffer.data(), recvBytes);
+				}
+
+				return recvBytes;
+			}
+			else
+				return 0;
+		}
+		else
+			return 0;
+	}
+
+	/*
+	Call this function if the expected data is a normal ASCII string, or a Unicode string.
+	Returns the number of bytes received.
+	Arguments:
+	  - strData:    Reference to the string variable used. Examples: std::string, std::wstring, std::u16string, std::u32string.
+	  - senderIP:   The IP address of the sender.
+	  - senderPort: The sender's port number.
+	*/
+	template<typename T>
+	int ReceiveDataUDP(std::basic_string<T>& strData, std::string& senderIP, uint16_t& senderPort)
+	{
+		if (thisSocketInfo.Protocol == IPPROTO::IPPROTO_UDP)
+		{
+			int udpSenderLen = sizeof(sockaddr);
+			SocketInfo& senderInfo = thisSocketInfo.RemoteSocketInfo.front();
+
+			memset(rxBuffer.data(), '\0', rxBuffer.size());
+			recvBytes = recvfrom(thisSocketInfo.SocketFD, (char*)rxBuffer.data(), (int)rxBuffer.size(), 0, (sockaddr*)&senderInfo.Addr, &udpSenderLen);
+			
+			GetIPFromSockAddr(senderInfo);
+			senderIP = senderInfo.IPAddress;
+			GetPortFromSockAddr(senderInfo);
+			senderPort = senderInfo.Port;
+
+			if ((recvBytes > 0) && (recvBytes != SOCKET_ERROR))
+			{
+				strData.assign((T*)rxBuffer.data());
+				return recvBytes;
+			}
+			else
+				return 0;
+		}
+		else
+			return 0;
+	}
+
+	/*
+	Call this function if the expected data is a normal ASCII string, or a Unicode string.
+	Returns the number of bytes received.
+	Arguments:
+	  - strData: Reference to the string variable used. Examples: std::string, std::wstring, std::u16string, std::u32string.
+	*/
+	template<typename T>
+	int ReceiveDataUDP(std::basic_string<T>& strData)
+	{
+		if (thisSocketInfo.Protocol == IPPROTO::IPPROTO_UDP)
+		{
+			int udpSenderLen = sizeof(sockaddr);
+			SocketInfo& senderInfo = thisSocketInfo.RemoteSocketInfo.front();
+
+			memset(rxBuffer.data(), '\0', rxBuffer.size());
+			recvBytes = recvfrom(thisSocketInfo.SocketFD, (char*)rxBuffer.data(), (int)rxBuffer.size(), 0, (sockaddr*)&senderInfo.Addr, &udpSenderLen);
+
+			if ((recvBytes > 0) && (recvBytes != SOCKET_ERROR))
+			{
+				strData.assign((T*)rxBuffer.data());
+				return recvBytes;
+			}
+			else
+				return 0;
+		}
+		else
+			return 0;
+	}
 	
-	uint16_t GetTxBufferSize(SOCKET socket);
-	void SetTxBufferSize(SOCKET socket, const uint16_t& newSize);
 
-	uint16_t& GetMaxTCPConnections(SOCKET socket);
-	void SetMaxTCPConnections(SOCKET socket, const uint16_t& maxNum);
+#elif SOCKET_PLATFORM_UNIX
+	//Initialize socket
+	uint16_t Init(const char* localIP, const uint16_t& localPort, const SocketType& type, const bool& isNonBlocking)
+	{
+		//TODO
+	}
 
-	bool ConnectToTCPServer(SOCKET socket, const char* serverIP, const uint16_t& serverPort);
+	//Close the socket
+	bool CloseSocket()
+	{
+		return (close(localSocket) == 0);
+	}
 
-	bool SendData(SOCKET socket, const IPPROTO& protocol, void* data, const uint16_t& dataSize, const char* targetIP, const uint16_t& targetPort);
-	
-	uint16_t CheckReceivedBytes(SOCKET socket, const IPPROTO& protocol);
-	uint16_t ReceiveData(SOCKET socket, const IPPROTO& protocol, void* data, const uint16_t& dataSize);
-	uint16_t ReceiveData(SOCKET socket, const IPPROTO& protocol, void* data, const uint16_t& dataSize, std::string& senderIP, uint16_t& senderPort);
-
+	//Check number of bytes waiting in the input buffer
+	inline uint16_t CheckReceivedBytes()
+	{
+		ioctl(localSocket, FIONREAD, &recvBytes);
+		return recvBytes;
+	}
+#endif
 
 protected:
+	//Get receive buffer size
+	const uint32_t& GetRxBufferSize() const
+	{
+		return (uint32_t)rxBuffer.size();
+	}
+
+	//Set receive buffer size
+	void SetRxBufferSize(const uint32_t& newSize)
+	{
+		if (newSize != rxBuffer.size())
+			rxBuffer.resize((newSize > maxBufferSize) ? maxBufferSize : newSize);
+	}
+
+	//Get an IP address of a socket in string representation
+	inline void GetIPFromSockAddr(SocketInfo& socket)
+	{
+		if (socket.IPAddress.size() != INET_ADDRSTRLEN)
+			socket.IPAddress.resize(INET_ADDRSTRLEN);
+
+		inet_ntop(AF_INET, &socket.Addr.sin_addr, &socket.IPAddress[0], INET_ADDRSTRLEN);
+	}
+	
+	//Get the port number of a socket
+	inline void GetPortFromSockAddr(SocketInfo& socket) const
+	{
+		socket.Port = ntohs(socket.Addr.sin_port);
+	}
+
+	template<typename T>
+	inline bool IsUnicodeString(T* data)
+	{
+		size_t type = typeid(T).hash_code();
+
+		return (
+			(type == typeid(wchar_t).hash_code()) ||
+			(type == typeid(char16_t).hash_code()) ||
+			(type == typeid(char32_t).hash_code())
+			);
+	}
 
 private:
-	std::map<SOCKET, UDPSocketStruct> lvUDPSocketMap;	//(Local Handle, Socket Data)
-	std::map<SOCKET, TCPSocketStruct> lvTCPSocketMap;	//(Local Handle, Socket Data)
-
-	inline bool ResizeBuffer(char* buffer, uint16_t& size, const uint16_t& newSize);
+	LocalSocketInfo thisSocketInfo;
+	sockaddr_in		udpTargetAddr;
+	
+	std::vector<unsigned char> rxBuffer, txBuffer;
+	uint32_t	maxBufferSize = MAX_UDP_BUFFER_SIZE;
+	int recvBytes = 0;
 };
-#endif //_NET_SOCKET_MANAGER_H_
+#endif //_NETSOCKET_H_
---
+++ NetUtil.h
@@ -0,0 +1,103 @@
+//Copyright(c) 2022 Jashen Low
+//
+//Permission is hereby granted, free of charge, to any person obtaining
+//a copy of this softwareand associated documentation files(the
+//	"Software"), to deal in the Software without restriction, including
+//	without limitation the rights to use, copy, modify, merge, publish,
+//	distribute, sublicense, and /or sell copies of the Software, and to
+//	permit persons to whom the Software is furnished to do so, subject to
+//	the following conditions :
+//
+//The above copyright noticeand this permission notice shall be
+//included in all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+//LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+//WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#pragma once
+#ifndef _NETUTIL_H_
+#define _NETUTIL_H_
+
+#include <string>
+
+/*
+* NOTES:
+*   - This class serves as a platform independent implementation for performing byte-swapping operations.
+*/
+class NetUtil
+{
+public:
+	//Call this to perform byte-swapping on primitive data types.
+	template<typename T>
+	static bool ByteSwap(T& data)
+	{
+		if (CheckValidType(data))
+		{
+			char* buff		= (char*)&data;
+			uint16_t size	= sizeof(T);
+			
+			for (uint16_t i = 0; i < (size / 2); i++)
+				std::swap(buff[i], buff[size - 1 - i]);
+
+			return true;
+		}
+		else
+			return false;
+	}
+
+	//Call this for byte-swapping unicode strings directly from the std container.
+	template<typename T>
+	static bool ByteSwap(std::basic_string<T>& str)
+	{
+		//Ignore std::string since it uses the char type.
+		if (typeid(T).hash_code() != typeid(char).hash_code())
+		{
+			char* buff			= (char*)&str[0];
+			uint16_t charSize	= sizeof(T);
+			
+			for (uint16_t i = 0; i < (str.length() * charSize); i += charSize)
+			{
+				for (uint16_t c = 0; c < (charSize / 2); c++)
+					std::swap(buff[i + c], buff[i + charSize - 1 - c]);
+			}
+
+			return true;
+		}
+		else
+			return false;
+	}
+
+private:
+	NetUtil() {}
+	~NetUtil() {}
+
+	template<typename T>
+	static bool CheckValidType(const T& data)
+	{
+		size_t type = typeid(T).hash_code();
+
+		return (
+			(type == typeid(short).hash_code()) ||
+			(type == typeid(unsigned short).hash_code()) ||
+			(type == typeid(int).hash_code()) ||
+			(type == typeid(unsigned int).hash_code()) ||
+			(type == typeid(long).hash_code()) ||
+			(type == typeid(unsigned long).hash_code()) ||
+			(type == typeid(long long).hash_code()) ||
+			(type == typeid(unsigned long long).hash_code()) ||
+			(type == typeid(float).hash_code()) ||
+			(type == typeid(double).hash_code()) ||
+			(type == typeid(long double).hash_code()) ||
+			(type == typeid(wchar_t).hash_code()) ||
+			(type == typeid(char16_t).hash_code()) ||
+			(type == typeid(char32_t).hash_code())
+			);
+	}
+};
+
+#endif
