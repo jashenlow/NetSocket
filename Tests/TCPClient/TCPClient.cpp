46a6a83bbc755d3bb53e924ffacac052a7a907b4
Jashen Low <jashenlow@gmail.com>
2022-08-20 16:56:00 +0800

    [Updated]   [Tests]     - TCPClient now uses an argument to define the local IP address.
    Signed-off-by: Jashen Low <jashenlow@gmail.com>

--- Tests/TCPClient/TCPClient.cpp
+++ Tests/TCPClient/TCPClient.cpp
@@ -10,172 +10,182 @@
 int main(int argc, char** argv)
 {
     //_setmode(_fileno(stdout), _O_U16TEXT); //To set console in unicode.
+    std::string ipAddress;
 
-    NetSocket testSocket;
-    WinsockError errorCode;
+    if (argv[1] != nullptr)
+        ipAddress = argv[1];
 
-    errorCode = testSocket.Init("192.168.10.5", 80, SocketType::TCP_CLIENT, false); //Change IP to console argument.
-
-    if (errorCode == WinsockError::OK)
+    //Check for empty string
+    if (!ipAddress.empty())
     {
-        const NetSocket::LocalSocketInfo& localInfo = testSocket.GetThisSocketInfo();
+        NetSocket testSocket;
+        WinsockError errorCode;
 
-        printf("\tLocal HostName: %s\n", localInfo.HostName);
-        printf("\tLocal IP: %s\n", localInfo.IPAddress.c_str());
-        printf("\tLocal Port: %d\n", localInfo.Port);
+        errorCode = testSocket.Init(ipAddress.c_str(), 80, SocketType::TCP_CLIENT, false); //Change IP to console argument.
 
-        std::string strURL = "httpbin.org";
-
-        bool isConnected = false;
-        int errConnect = testSocket.ConnectToServerTCP(strURL.c_str(), 80);
-
-        if (errConnect == ConnectingErrCode)
+        if (errorCode == WinsockError::OK)
         {
-            int errWaitConnect = testSocket.CheckForConnectNonBlockTCP(2.0);
+            const NetSocket::LocalSocketInfo& localInfo = testSocket.GetThisSocketInfo();
 
-            switch (errWaitConnect)
+            printf("\tLocal HostName: %s\n", localInfo.HostName);
+            printf("\tLocal IP: %s\n", localInfo.IPAddress.c_str());
+            printf("\tLocal Port: %d\n", localInfo.Port);
+
+            std::string strURL = "httpbin.org";
+
+            bool isConnected = false;
+            int errConnect = testSocket.ConnectToServerTCP(strURL.c_str(), 80);
+
+            if (errConnect == ConnectingErrCode)
             {
-                case 1:
-                    printf("Connected to %s\n", strURL.c_str());
-                    isConnected = true;
-                    break;
-                case 0:
-                    printf("Connection to %s timeout expired.\n", strURL.c_str());
-                    break;
-                case SOCKET_ERROR:
-                    printf("Failed to connect to %s\n", strURL.c_str());
-                    break;
-            }
-        }
-        else if (errConnect == 0)
-        {
-            isConnected = true;
-            printf("Connected to %s\n", strURL.c_str());
-        }
-        else
-            printf("Connection to %s failed.\n", strURL.c_str());
+                int errWaitConnect = testSocket.CheckForConnectNonBlockTCP(2.0);
 
-        if (isConnected)
-        {
-            const NetSocket::SocketInfo& serverInfo = testSocket.GetRemoteSocketInfo().front();
-
-            printf("\tHostName: %s\n", serverInfo.HostName);
-            printf("\tServer IP: %s\n", serverInfo.IPAddress.c_str());
-            printf("\tServer Port: %d\n", serverInfo.Port);
-            printf("\n");
-
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
-
-            std::string request;
-            request += "GET / HTTP/1.1\r\n";
-            request += "Host: " + strURL + "\r\n";
-            request += "Connection: close\r\n\r\n";
-
-            if (testSocket.SendToServerTCP(request.c_str(), request.length() + 1))
-            {
-                constexpr const char* RESP_HEADER_END = "\r\n";
-                int recvBytes = 0;
-                int totalRecvBytes = 0;
-                size_t contentLength = 0;
-                std::stringstream ss;
-                double timeoutTimer = 0.0;
-                bool fullHeaderReceived = false;
-
-                //Read response header
-                while (recvBytes <= 0)
+                switch (errWaitConnect)
                 {
-                    recvBytes = testSocket.ReceiveFromServerTCP(ss);
-                    
-                    if (recvBytes > 0)
+                    case 1:
+                        printf("Connected to %s\n", strURL.c_str());
+                        isConnected = true;
+                        break;
+                    case 0:
+                        printf("Connection to %s timeout expired.\n", strURL.c_str());
+                        break;
+                    case SOCKET_ERROR:
+                        printf("Failed to connect to %s\n", strURL.c_str());
+                        break;
+                }
+            }
+            else if (errConnect == 0)
+            {
+                isConnected = true;
+                printf("Connected to %s\n", strURL.c_str());
+            }
+            else
+                printf("Connection to %s failed.\n", strURL.c_str());
+
+            if (isConnected)
+            {
+                const NetSocket::SocketInfo& serverInfo = testSocket.GetRemoteSocketInfo().front();
+
+                printf("\tHostName: %s\n", serverInfo.HostName);
+                printf("\tServer IP: %s\n", serverInfo.IPAddress.c_str());
+                printf("\tServer Port: %d\n", serverInfo.Port);
+                printf("\n");
+
+                std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+                std::string request;
+                request += "GET / HTTP/1.1\r\n";
+                request += "Host: " + strURL + "\r\n";
+                request += "Connection: close\r\n\r\n";
+
+                if (testSocket.SendToServerTCP(request.c_str(), request.length() + 1))
+                {
+                    constexpr const char* RESP_HEADER_END = "\r\n";
+                    int recvBytes = 0;
+                    int totalRecvBytes = 0;
+                    size_t contentLength = 0;
+                    std::stringstream ss;
+                    double timeoutTimer = 0.0;
+                    bool fullHeaderReceived = false;
+
+                    //Read response header
+                    while (recvBytes <= 0)
                     {
-                        std::string recvStr = ss.str();
-                        printf("%s\n", recvStr.c_str());
+                        recvBytes = testSocket.ReceiveFromServerTCP(ss);
 
-                        fullHeaderReceived = (recvStr.substr(recvStr.length() - 2, 2) == RESP_HEADER_END);
-
-                        if (fullHeaderReceived)
+                        if (recvBytes > 0)
                         {
-                            bool isResponse = false;
-                            std::string lineStr;
-                            
-                            while (std::getline(ss, lineStr))
-                            {
-                                if (lineStr.find("HTTP") != std::string::npos)
-                                    isResponse = true;                               
+                            std::string recvStr = ss.str();
+                            printf("%s\n", recvStr.c_str());
 
-                                if (isResponse)
+                            fullHeaderReceived = (recvStr.substr(recvStr.length() - 2, 2) == RESP_HEADER_END);
+
+                            if (fullHeaderReceived)
+                            {
+                                bool isResponse = false;
+                                std::string lineStr;
+
+                                while (std::getline(ss, lineStr))
                                 {
-                                    //Look for "Content-Length"
-                                    if (lineStr.find("Content-Length") != std::string::npos)
+                                    if (lineStr.find("HTTP") != std::string::npos)
+                                        isResponse = true;
+
+                                    if (isResponse)
                                     {
-                                        size_t delimPos = lineStr.find(':');
-                                        if (delimPos != std::string::npos)
+                                        //Look for "Content-Length"
+                                        if (lineStr.find("Content-Length") != std::string::npos)
                                         {
-                                            size_t numberPos = delimPos + 2;
-                                            contentLength = strtol(lineStr.substr(numberPos, lineStr.length() - numberPos).c_str(), nullptr, 10);
-                                            break;
+                                            size_t delimPos = lineStr.find(':');
+                                            if (delimPos != std::string::npos)
+                                            {
+                                                size_t numberPos = delimPos + 2;
+                                                contentLength = strtol(lineStr.substr(numberPos, lineStr.length() - numberPos).c_str(), nullptr, 10);
+                                                break;
+                                            }
+                                            else
+                                            {
+                                                printf("Unable to find content length from header. Exiting...\n");
+                                                break;
+                                            }
                                         }
-                                        else
-                                        {
-                                            printf("Unable to find content length from header. Exiting...\n");
-                                            break;
-                                        }
-                                    } 
-                                }
-                                else
-                                {
-                                    printf("Unrecognized response received. Exiting...\n");
-                                    break;
+                                    }
+                                    else
+                                    {
+                                        printf("Unrecognized response received. Exiting...\n");
+                                        break;
+                                    }
                                 }
                             }
                         }
-                    }
-                    else
-                    {
-                        timeoutTimer += (10.0 / 1000.0);
-
-                        if (timeoutTimer >= 5.0)
+                        else
                         {
-                            printf("Failed to get response. Exiting...\n");
-                            break;
+                            timeoutTimer += (10.0 / 1000.0);
+
+                            if (timeoutTimer >= 5.0)
+                            {
+                                printf("Failed to get response. Exiting...\n");
+                                break;
+                            }
+
+                            std::this_thread::sleep_for(std::chrono::milliseconds(10));
                         }
-
-                        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-                    } 
-                }
-
-                //Read page content
-                std::string contentStr;
-                timeoutTimer = 0.0;
-
-                while (contentStr.length() != contentLength)
-                {
-                    recvBytes = testSocket.ReceiveFromServerTCP(contentStr);
-
-                    if (contentStr.length() != contentLength)
-                    {
-                        timeoutTimer += (10.0 / 1000.0);
-
-                        if (timeoutTimer >= 5.0)
-                        {
-                            printf("Failed to get content. Exiting...\n");
-                            break;
-                        }
-
-                        std::this_thread::sleep_for(std::chrono::milliseconds(10));
                     }
-                    else
-                        break;
+
+                    //Read page content
+                    std::string contentStr;
+                    timeoutTimer = 0.0;
+
+                    while (contentStr.length() != contentLength)
+                    {
+                        recvBytes = testSocket.ReceiveFromServerTCP(contentStr);
+
+                        if (contentStr.length() != contentLength)
+                        {
+                            timeoutTimer += (10.0 / 1000.0);
+
+                            if (timeoutTimer >= 5.0)
+                            {
+                                printf("Failed to get content. Exiting...\n");
+                                break;
+                            }
+
+                            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+                        }
+                        else
+                            break;
+                    }
+
+                    printf("%s\n", contentStr.c_str());
                 }
-                           
-                printf("%s\n", contentStr.c_str());
             }
         }
+        else
+            printf("Error initializing socket.\n");
+
+        testSocket.CloseThisSocket();
+        system("pause");
     }
     else
-        printf("Error initializing socket.\n");
-
-    testSocket.CloseThisSocket();
-    system("pause");
+        printf("Error: Please input your machine's IP address as an argument.\n");
 }
 
